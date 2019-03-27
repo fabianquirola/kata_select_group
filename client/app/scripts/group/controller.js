0e30a70c29da5a4ec9c580b8be744658f7f3f429
fabianquirola <fabianquirola@hotmail.com>
2019-03-26 23:30:21 -0500

    Se verificaron los casos enviados.

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -1,23 +1,88 @@
 'use strict';
 
 angular.module('Group')
-.controller('group', function ($scope) {
+    .controller('group', function ($scope) {
 
-  $scope.controller_loaded = 'Group loaded!';
+        $scope.controller_loaded = 'Group loaded!';
 
-  $scope.registro = function(p1,p2){
-    console.log('pares 1 =', p1, 'pares 2 =', p2);
-    
-    return [2011];
-  };
+        $scope.resolverLista = function (pEst, pLon) {
+            console.log('pares 1 =', pEst, 'pares 2 =', pLon);
+
+            //lista para resultado
+            var list = [];
+
+            if (pEst.length === pLon.length) {
+
+                //longitud de pares
+                var ln = pEst.length;
+
+                //lista pares de Estocolmo
+                var lpEst = {};
+
+                //lista pares de Londres
+                var lpLon = {};
+
+                //guardare como objeto cuantas veces se repite dicho par
+
+                pEst.forEach(function (x) {
+                    lpEst[x] = (lpEst[x] || 0) + 1;
+                });
+
+                pLon.forEach(function (x) {
+                    lpLon[x] = (lpLon[x] || 0) + 1;
+                });
+
+                for (var i = 0; i < ln; i++) {
+
+                    var ppEst = pEst[i];
+                    var ppLon = pLon[i];
+
+                    if (list.indexOf(ppEst) === -1 && list.indexOf(ppLon) === -1) {
+
+                        if (lpEst[ppEst] > lpLon[ppLon]) {
+                            //cuando el par de estocolmo tiene m'as parejas
+                            list.push(ppEst);
+                        } else if (lpEst[ppEst] < lpLon[ppLon]) {
+                            //cuando el par de londres tiene m'as parejas
+                            list.push(ppLon);
+
+                        } else {
+                            //cuando tienen iguales se usara un random;
+
+                            var random = Math.random() >= 0.5;
+
+                            if (random) {
+                                list.push(ppEst);
+                            } else {
+                                list.push(ppLon);
+                            }
+
+                        }
+
+                    } else {
+                        console.log('Alguno de la pareja ya esta en lista');
+                    }
+
+                }
+
+                console.log(lpEst);
+                console.log(lpLon);
+
+                console.log(list);
 
 
+            } else {
+                return console.error('Debe enviar los datos completos');
+            }
+            return list;
+        };
 
-})
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/group', {
-    templateUrl: 'scripts/group/views/group.html',
-    controller: 'group'
-  });
-});
+
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/group', {
+                templateUrl: 'scripts/group/views/group.html',
+                controller: 'group'
+            });
+    });
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -2,60 +2,67 @@
 
 describe('Controller: select group', function () {
 
-  beforeEach(module('Group'));
+    beforeEach(module('Group'));
 
-  var controller;
-  var scope;
+    var controller;
+    var scope;
 
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('group', { $scope: scope });
-  }));
-
-  describe('On instance', function () {
-    it('should set "controller_loaded" variable in scope', function () {
-      expect(scope.controller_loaded).toContain('loaded');
-    });
-    
-    fit('should return case 1', function () {
-      var result = scope.registro([1009 ,1017],[2011,2011]);
-
-      expect(result).toEqual([2011]);
-    });
-    
-    it('should return case 2', function () {
-      
-    });
-  });
-
-  describe('when going to /group', function () {
-
-    var route, location, rootScope, httpBackend;
-
-    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
-      route = $route;
-      location = $location;
-      rootScope = $rootScope;
-      httpBackend = $httpBackend;
-
-      httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('group', {$scope: scope});
     }));
 
-    afterEach(function () {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    describe('On instance', function () {
+        /*it('should set "controller_loaded" variable in scope', function () {
+          expect(scope.controller_loaded).toContain('loaded');
+        });
+        */
+        it('should return case 1', function () {
+            var result = scope.resolverLista([1009, 1017], [2011, 2011]);
+
+            expect(result).toEqual([2011]);
+        });
+
+        it('should return case 2', function () {
+            var result = scope.resolverLista([1009, 1009, 1002, 1003], [2000, 2001, 2002, 2002]);
+
+            expect(result).toEqual([1009, 2002]);
+        });
+        /*
+        it('should return case 2', function () {
+
+        });
+        */
     });
 
-    it('should use minesweeper.html and controller', function () {
-      expect(route.current).toBeUndefined();
+    describe('when going to /group', function () {
 
-      location.path('/group');
+        var route, location, rootScope, httpBackend;
 
-      httpBackend.flush();
+        beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
+            route = $route;
+            location = $location;
+            rootScope = $rootScope;
+            httpBackend = $httpBackend;
 
-      expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
-      expect(route.current.controller).toBe('group');
+            httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+        }));
+
+        afterEach(function () {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should use minesweeper.html and controller', function () {
+            expect(route.current).toBeUndefined();
+
+            location.path('/group');
+
+            httpBackend.flush();
+
+            expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
+            expect(route.current.controller).toBe('group');
+        });
     });
-  });
 
 });
